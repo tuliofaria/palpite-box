b737d2f5ab11c96db0d1b4bd6826526ac84c38ab
Tulio Faria <tuliofaria@gmail.com>
2020-07-03 01:23:37 -0300

    fix.

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -3,12 +3,11 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 const fromBase64 = value => {
-  const buff = new Buffer(value, 'base64');
+  const buff = Buffer.from(value, 'base64');
   return buff.toString('ascii');
 }
 
 export default async (req, res) => {
-  console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
